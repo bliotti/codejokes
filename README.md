5d4879127e12686828c02f1c0f22367c288f02f0
Brian Liotti <bliotti@protonmail.com>
2019-11-16 22:36:57 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# codejokes
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './styles.css'
+
+function App() {
+  const aa = [
+    'https://hackernoon.com/Devimg/logo-hn.png',
+    'https://hackernoon.com/topnav/seen_white_cropped.png',
+    'https://hackernoon.com/hn-images/1*_GNHlSp-ctornHauDDiBiA.jpeg',
+    'https://hackernoon.com/hn-images/1*Y0tJ_VOlSidCqBIT9TTkyg.jpeg',
+    'https://hackernoon.com/hn-images/1*qdsOHGtDPsXHDb7GZ6X8CQ.gif',
+    'https://hackernoon.com/hn-images/1*c9zuInVQFcs6gtk4ZVxBlA.jpeg',
+    'https://hackernoon.com/hn-images/1*PdKeUpPIa9ApLb2hcvnPXw.png',
+    'https://hackernoon.com/hn-images/1*tO2UvhOdAdZpKKZOj2BA-g.png',
+    'https://hackernoon.com/hn-images/1*MAQ2E9rXy7DwaZPX5K78wA.jpeg',
+    'https://hackernoon.com/hn-images/1*yM0ipmW5JP41ee-m5Q7KPQ.jpeg',
+    'https://hackernoon.com/hn-images/1*NSVfi_R4_UrJ12PRJ6Rg5Q.jpeg',
+    'https://hackernoon.com/hn-images/1*7eXoCoTbeFYtWqnmxLJX0g.jpeg',
+    'https://hackernoon.com/hn-images/1*t3E9-ifBK5Es39pIox9tCA.png',
+    'https://hackernoon.com/hn-images/1*oy7H0sovZhPjY17f5G6fUw.png',
+    'https://hackernoon.com/hn-images/1*rtRrPznqx1iWKtmJJ_oiVQ.png',
+    'https://hackernoon.com/hn-images/1*tGFyroolzUR7E0gBoiY11w.png',
+    'https://hackernoon.com/hn-images/1*TPiMsWm4PHQ4NXtCRHRZ6g.png',
+    'https://hackernoon.com/hn-images/1*jdbZH4jl7CteKX5Jru_HpA.jpeg',
+    'https://hackernoon.com/hn-images/1*hrBszBQU8Fh30OowYNufrw.jpeg',
+    'https://hackernoon.com/hn-images/1*j80tBUC6gFi6E6mCqGUTrw.jpeg',
+    'https://hackernoon.com/hn-images/1*CtIdTEzKtQo-9Bc9ljArEA.png',
+    'https://i.ytimg.com/vi/QMYfkOtYYlg/hqdefault.jpg',
+    'https://hackernoon.com/hn-images/1*ZWiLdOvoBJfothi4Z1zj2A.jpeg',
+    'https://hackernoon.com/hn-images/1*M9_LQ_1pWnmW3zdKXXu_BA.png',
+    'https://hackernoon.com/hn-images/1*PtmNHubOdr0Cd1A4p8T2kg.jpeg',
+    'https://hackernoon.com/hn-images/1*3IZuJIKnn5EYF86_uMxn-w.png',
+    'https://hackernoon.com/hn-images/1*NlbZfXAMZB7zBwuV_fNLug.png',
+    'https://hackernoon.com/hn-images/1*SYT31XMNWdEPp1ogEJPRVw.png',
+    'https://hackernoon.com/hn-images/1*uJoNo--Z68WoX6FASkR_pg.png',
+    'https://hackernoon.com/hn-images/1*Rl0NfPha2vlubHDuY5FMaw.png',
+    'https://hackernoon.com/hn-images/1*VsTynocxe5Fu83dNo2YPGg.png',
+    'https://hackernoon.com/hn-images/1*3AKcl05VwTCqxG4rQBf-SA.png',
+    'https://ucarecdn.com/57662ddc-da7c-407b-afcb-cb45184b2705/',
+    'https://hackernoon.com/Devimg/hn-logo.png',
+  ]
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      {aa.map((x, i) => (
+        <img width={45 * (i % 2).toString()} height="45" alt={`${x}`} src={x} />
+      ))}
+    </div>
+  )
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
